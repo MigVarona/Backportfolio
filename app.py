c3c39b5b42f76ebf732b61c205e508d7fefc0aa7
MigVarona <migvaronag@gmail.com>
2024-06-16 23:40:20 +0200

    Email service funcionando

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from flask_mail import Mail, Message
 from config import Config
 import os
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+mail = Mail(app)
 mongo = PyMongo(app)
 db = mongo.db
 
-# Define the Project schema
+CORS(app)  # Habilita CORS para toda la aplicación
+
+
 class Project:
     def __init__(self, title, description, technologies, image, link, github, technologies2):
         self.title = title
@@ -84,6 +90,38 @@ def update_project(id):
 def delete_project(id):
     db.projects.delete_one({"_id": ObjectId(id)})
     return jsonify({"message": "Project deleted"})
+
+@app.route('/send-email', methods=['POST'])
+def send_email():
+    data = request.get_json()
+    subject = "Mensaje de contacto" 
+    sender = app.config['MAIL_USERNAME']  
+    recipients = [data['email']] 
+
+    # Crea el mensaje de correo
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = f"Nombre: {data['name']}\nEmail: {data['email']}\n\nMensaje:\n{data['message']}"
+
+    try:
+        mail.send(msg)  # Envía el correo
+        return jsonify({"message": "Email sent"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/test-email', methods=['GET'])
+def test_email():
+    try:
+        msg = Message(
+            subject="Test Email",
+            sender=app.config['MAIL_USERNAME'],
+            recipients=[app.config['MAIL_USERNAME']],  # Enviando a ti mismo para probar
+            body="This is a test email sent from Flask."
+        )
+        mail.send(msg)
+        return jsonify({"message": "Email sent successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
