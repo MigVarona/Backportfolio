2575fdb52695b1979814c709b3f0c78186e747a3
MigVarona <migvaronag@gmail.com>
2024-06-17 11:58:55 +0200

    pr

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-# Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
 class Config:
-    MONGO_URI = os.getenv('MONGO_URI') or 'mongodb+srv://miguel:MIKy1969@cluster0.3hlzcty.mongodb.net/portfolio?retryWrites=true&w=majority'
+    MONGO_URI = os.getenv('MONGO_URI') 
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
     MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') == 'True'
